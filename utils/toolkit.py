338d326df054df4d661437a6c5b5ff918a7c1f53
alfolegunda <alfolegunda@gmail.com>
2024-06-17 14:58:47 -0400

    add toolkit

---
+++ utils/toolkit.py
@@ -0,0 +1,63 @@
+def annualized_rets(r, periods_per_year):
+    """
+    Annualizes a set of return
+    """
+
+    compounded_growth = (1 + r).prod()
+    n_periods = r.shape[0]
+    return compounded_growth ** (periods_per_year / n_periods) - 1
+
+
+def annualized_vol(r, periods_per_year):
+    """
+    Annualizes the vol of a set returns
+    """
+
+    return r.std() * (periods_per_year ** 0.5)
+
+
+def sharpe_ratio(r, riskfree_rate, periods_per_year):
+    """
+    Computes the annualized sharpe ratio of a set of returns
+    """
+    # convert the annual riskfree rate to per period
+    rf_per_period = (1 + riskfree_rate) ** (1 / periods_per_year) - 1
+    excess_ret = r - rf_per_period
+    ann_ex_ret = annualized_rets(excess_ret, periods_per_year)
+    ann_vol = annualized_vol(r, periods_per_year)
+    return ann_ex_ret / ann_vol
+
+
+def semideviation(r):
+    """
+    Returns the semideviation aka negative semideviation of r
+    r must be a series or DataFrame
+    """
+    is_negative = r < 0
+    return r[is_negative].std(ddof=0)
+
+
+def skewness(r):
+    """
+    Alternative to scipy.stats.skew()
+    Computes the skewness of the supplied Series or DataFrame
+    Returns a float or a Series
+    """
+    demeaned_r = r - r.mean()
+    # use the population standard deviation, so set dof=0
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r ** 3).mean()
+    return exp / sigma_r ** 3
+
+
+def kurtosis(r):
+    """
+    Alternative to scipy.stats.kurtosis()
+    Computes the kurtosis of the supplied Series or DataFrame
+    Returns a float or a Series
+    """
+    demeaned_r = r - r.mean()
+    # use the population standard deviation, so set dof=0
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r ** 4).mean()
+    return exp / sigma_r ** 4
