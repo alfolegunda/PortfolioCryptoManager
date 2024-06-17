849bd439bf77feeddd23abb45c40dab8e22d553c
alfolegunda <alfolegunda@gmail.com>
2024-06-17 14:57:46 -0400

    generalize function of comparison

--- utils/graficos.py
+++ utils/graficos.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 
-def graficar_comparacion(fechas, valores_portafolio, valores_portafolio_eth):
+def graficar_comparacion(df_portafolios):
     plt.figure(figsize=(12, 6))
-    plt.plot(fechas, valores_portafolio, label='Portafolio Actual')
-    plt.plot(fechas, valores_portafolio_eth, label='Portafolio ETH')
+
+    fechas = df_portafolios.index
+
+    for column in df_portafolios:
+        plt.plot(fechas, df_portafolios[column], label=column)
+
     plt.title('Comparación del Valor del Portafolio a lo Largo del Tiempo')
     plt.xlabel('Fecha')
     plt.ylabel('Valor del Portafolio (USD)')
